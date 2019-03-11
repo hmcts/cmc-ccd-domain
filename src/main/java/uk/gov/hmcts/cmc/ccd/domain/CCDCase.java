27d72a046ed31d00e7d59fc46bc1d32393f6fbe4
alexis.gayte <alexis.gayte@gmail.com>
2019-03-11 16:58:09 +0000

    add first copy from claim-store

---
+++ src/main/java/uk/gov/hmcts/cmc/ccd/domain/CCDCase.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.cmc.ccd.domain;
+
+import uk.gov.hmcts.cmc.ccd.domain.defendant.CCDDefendant;
+import uk.gov.hmcts.cmc.ccd.domain.evidence.CCDEvidenceRow;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+public class CCDCase {
+
+    private Long id;
+    private String referenceNumber;
+    private String externalId;
+    private String reason;
+    private String feeCode;
+    private String feeAccountNumber;
+    private BigInteger feeAmountInPennies;
+    private String externalReferenceNumber;
+    private AmountType amountType;
+    private BigDecimal amountLowerValue;
+    private BigDecimal amountHigherValue;
+    private List<CCDCollectionElement<CCDAmountRow>> amountBreakDown;
+    private BigDecimal totalAmount;
+    private CCDNotKnown notKnown;
+    private CCDInterestType interestType;
+    private BigDecimal interestBreakDownAmount;
+    private String interestBreakDownExplanation;
+    private BigDecimal interestRate;
+    private String interestReason;
+    private BigDecimal interestSpecificDailyAmount;
+    private CCDInterestDateType interestDateType;
+    private LocalDate interestClaimStartDate;
+    private String interestStartDateReason;
+    private CCDInterestEndDateType interestEndDateType;
+    private String paymentId;
+    private BigDecimal paymentAmount;
+    private String paymentReference;
+    private String paymentStatus;
+    private LocalDate paymentDateCreated;
+    private String preferredCourt;
+    private String personalInjuryGeneralDamages;
+    private String housingDisrepairCostOfRepairDamages;
+    private String housingDisrepairOtherDamages;
+    private String sotSignerName;
+    private String sotSignerRole;
+    private List<CCDCollectionElement<CCDClaimant>> claimants;
+    private List<CCDCollectionElement<CCDDefendant>> defendants;
+    private List<CCDCollectionElement<CCDTimelineEvent>> timeline;
+    private List<CCDCollectionElement<CCDEvidenceRow>> evidence;
+}
---
+++ src/main/java/uk/gov/hmcts/cmc/ccd/domain/defendant/CCDDefendant.java
@@ -0,0 +1,54 @@
+package uk.gov.hmcts.cmc.ccd.domain.defendant;
+
+import uk.gov.hmcts.cmc.ccd.domain.CCDAddress;
+import uk.gov.hmcts.cmc.ccd.domain.CCDPartyType;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.time.LocalDate;
+
+@Value
+@Builder
+public class CCDDefendant {
+    private String letterHolderId;
+    private String defendantId;
+    private LocalDate responseDeadline;
+
+    private CCDPartyType claimantProvidedType;
+    private String claimantProvidedEmail;
+    private CCDAddress claimantProvidedServiceAddress;
+    private String claimantProvidedName;
+    private CCDAddress claimantProvidedAddress;
+    private CCDAddress claimantProvidedCorrespondenceAddress;
+    private LocalDate claimantProvidedDateOfBirth;
+    private String claimantProvidedContactPerson;
+    private String claimantProvidedCompaniesHouseNumber;
+    private String claimantProvidedTitle;
+    private String claimantProvidedBusinessName;
+
+    private String claimantProvidedRepresentativeOrganisationName;
+    private CCDAddress claimantProvidedRepresentativeOrganisationAddress;
+    private String claimantProvidedRepresentativeOrganisationPhone;
+    private String claimantProvidedRepresentativeOrganisationEmail;
+    private String claimantProvidedRepresentativeOrganisationDxAddress;
+
+    private String representativeOrganisationName;
+    private CCDAddress representativeOrganisationAddress;
+    private String representativeOrganisationPhone;
+    private String representativeOrganisationEmail;
+    private String representativeOrganisationDxAddress;
+
+    private CCDPartyType partyType;
+    private String partyTitle;
+    private String partyName;
+    private LocalDate partyDateOfBirth;
+    private String partyPhone;
+    private String partyEmail;
+    private CCDAddress partyAddress;
+    private CCDAddress partyCorrespondenceAddress;
+    private String partyBusinessName;
+    private String partyContactPerson;
+    private String partyCompaniesHouseNumber;
+
+}
