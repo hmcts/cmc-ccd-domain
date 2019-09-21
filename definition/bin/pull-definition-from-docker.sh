d48f6a2807e3b310396b02fd01500c0f59ba44dc
Dharmendra kumar <k_dharmendra@hotmail.com>
2019-09-21 09:01:24 +0100

    ROC-6025: Generate mi reports

--- definition/bin/pull-definition-from-docker.sh
+++ definition/bin/pull-definition-from-docker.sh
@@ -11,7 +11,7 @@ VERSION="${2:-}"
 
 case ${ENV} in
   local)
-    CLAIM_STORE_URL="http://claim-store-api:4400" # docker-compose service
+    CLAIM_STORE_URL="http://host.docker.internal:4400" # docker-compose service
   ;;
   saat|sprod|aat|ithc|prod|demo)
     CLAIM_STORE_URL="http://cmc-claim-store-${ENV}.service.core-compute-${ENV}.internal"
@@ -32,6 +32,6 @@ docker run --rm --name json2xlsx \
   sh -c "cd /opt/ccd-definition-processor && yarn json2xlsx -D /data/sheets -o /tmp/cmc-ccd.xlsx && yarn cache clean"
 
 echo "Versioning definition file..."
-mv ./definition/releases/cmc-ccd.xlsx ./definition/releases/cmc-ccd-v${VERSION}_$(echo $ENV | tr a-z A-Z).xlsx 
+mv ./definition/releases/cmc-ccd.xlsx ./definition/releases/cmc-ccd-v${VERSION}_$(echo $ENV | tr a-z A-Z).xlsx
 
 echo "Saved: ./definition/releases/cmc-ccd-v${VERSION}_$(echo $ENV | tr a-z A-Z).xlsx"
