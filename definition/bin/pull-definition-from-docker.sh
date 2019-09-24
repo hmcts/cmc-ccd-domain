52f6f78cc9c2505b5eb2fc5747538c89dcdbf24a
Dharmendra kumar <k_dharmendra@hotmail.com>
2019-09-24 14:00:07 +0100

    revert local test changes

--- definition/bin/pull-definition-from-docker.sh
+++ definition/bin/pull-definition-from-docker.sh
@@ -11,7 +11,7 @@ VERSION="${2:-}"
 
 case ${ENV} in
   local)
-    CLAIM_STORE_URL="http://host.docker.internal:4400" # docker-compose service
+    CLAIM_STORE_URL="http://claim-store-api:4400" # docker-compose service
   ;;
   saat|sprod|aat|ithc|prod|demo)
     CLAIM_STORE_URL="http://cmc-claim-store-${ENV}.service.core-compute-${ENV}.internal"
