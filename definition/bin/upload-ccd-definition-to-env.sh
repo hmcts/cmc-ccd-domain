bd90cfaae18497557a1967206ac91c4b47000d7a
justin-r-oliver <justin.r.oliver@gmail.com>
2020-05-28 17:24:03 +0100

    Add TransferContent.caseworkerName

--- definition/bin/upload-ccd-definition-to-env.sh
+++ definition/bin/upload-ccd-definition-to-env.sh
@@ -58,7 +58,7 @@ case ${ENV} in
     REDIRECT_URI=http://localhost:3451/oauth2redirect
     CCD_STORE_BASE_URL=http://host.docker.internal:4451
     AUTH_PROVIDER_BASE_URL=http://host.docker.internal:4552
-    CLAIM_STORE_URL=http://localhost:4400 # docker-compose service
+    CLAIM_STORE_URL=http://host.docker.internal:4400 # docker-compose service
     MICROSERVICE=ccd_gateway
   ;;
   perftest)
